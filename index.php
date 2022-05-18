88a8213b60890c1479e56c56cb8408e3750d856c
buchac-kodov-marek <safarikmarek147@gmail.com>
2022-05-18 11:42:08 +0200

    zmena1

--- index.php
+++ index.php
@@ -8,10 +8,51 @@
     <link rel="stylesheet" href="mystyle.css" />
   </head>
   <body>
-<a href="./player_read.php">get player</a>
-<br>
-<a href="./team_read.php">get team</a>
-<br>
-<a href="./player_create.html">create player</a>
+    <?php
+  $meno = $_SESSION["meno"];
+if(empty($meno)){
+    header("Location: ./formular.php");
+};
+
+$con = mysqli_connect("sql22.dnsserver.eu", "db15531xmarek", "Marek2022","db15531xmarek");
+// Check connection
+if (mysqli_connect_error())
+{
+echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+$result = mysqli_query($con,"SELECT * FROM lezitranz_db ORDER BY ID DESC");
+
+echo "<table border='1'>
+<thead>
+<tr>
+<th>ŠPZ</th>
+<th>Popis chyby</th>
+<th>Dátum a čas</th>
+<th>Stav opravy</th>
+<th>Dátum opravy</th>
+</tr>
+</thead>
+<tbody>";
+
+while($row = mysqli_fetch_array($result))
+{
+$ID = $row['ID'];
+echo "<tr>";
+echo "<td>" . $row['spz'] . "</td>";
+echo "<td>" . $row['porucha'] . "</td>";
+echo "<td>" . $row['datum'] . "</td>";
+echo "<td><form id='" .$ID. "' action='checked.php' method='POST'>
+<input type='hidden' name='id' value='$ID'>
+<select name='status1' id='stav' onchange='this.form.submit()'>" . $row['status'] . "</select>
+</form></td>";
+echo "<td>" . $row['opravene'] . "</td>";
+echo "</tr>";
+}
+echo "</tbody>
+</table>";
+mysqli_close($con);
+?>
+
   </body>
 </html>
